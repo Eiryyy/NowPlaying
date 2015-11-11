f79ce4590c0bdc64e3636a3790ddad289723e929
Eiryyy <xxx.eiryklav@gmail.com>
2015-11-12 01:46:52 +0900

    Initial Commit

---
+++ NowPlaying/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  NowPlaying
+//
+//  Created by Tsukasa on 2015/11/12.
+//  Copyright © 2015年 Tsukasa Kawagishi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
