9b092a4cd82879f02f254c731ccf77f4760beb8e
Eiryyy <xxx.eiryklav@gmail.com>
2015-11-12 19:20:45 +0900

    initial commit

---
+++ NowPlaying/EventMonitor.swift
@@ -0,0 +1,35 @@
+//
+//  EventMonitor.swift
+//  NowPlaying
+//
+//  Created by Tsukasa on 2015/11/12.
+//  Copyright © 2015年 Tsukasa Kawagishi. All rights reserved.
+//
+
+import Cocoa
+
+public class EventMonitor {
+    private var monitor: AnyObject?
+    private let mask: NSEventMask
+    private let handler: NSEvent? -> ()
+    
+    public init(mask: NSEventMask, handler: NSEvent? -> ()) {
+        self.mask = mask
+        self.handler = handler
+    }
+    
+    deinit {
+        stop()
+    }
+    
+    public func start() {
+        monitor = NSEvent.addGlobalMonitorForEventsMatchingMask(mask, handler: handler)
+    }
+    
+    public func stop() {
+        if monitor != nil {
+            NSEvent.removeMonitor(monitor!)
+            monitor = nil
+        }
+    }
+}
---
+++ NowPlaying/QuotesViewController.swift
@@ -0,0 +1,18 @@
+//
+//  QuotesViewController.swift
+//  NowPlaying
+//
+//  Created by Tsukasa on 2015/11/12.
+//  Copyright © 2015年 Tsukasa Kawagishi. All rights reserved.
+//
+
+import Cocoa
+
+class QuotesViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
